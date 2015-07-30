95acdeb8f221ca7f733cd5e5378baed8b3e757b3
Alphadelta14 <alpha@pokesplash.net>
2015-07-30 17:09:05 -0400

    Python: Added a setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+
+setup(name='servicediscovery',
+      version='1.0.0',
+      description='Service Discoverer',
+      url='http://github.com/Alphadelta14/python-servicediscovery',
+      author='Alpha',
+      author_email='alpha@projectpokemon.org',
+      license='MIT',
+      packages=['servicediscovery'],
+      zip_safe=True)
