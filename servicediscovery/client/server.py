9db01bfab9f1c6aa0da16b65a7ce6dedcf1b4b12
Alpha <alpha@pokesplash.net>
2015-07-30 00:09:28 -0400

    ServiceRegistry: Added API for client representation of the registry

---
+++ servicediscovery/client/server.py
@@ -0,0 +1,45 @@
+
+"""
+    ServiceRegistry
+    A client representation of the service registry
+
+    Author: Alpha <alpha@projectpokemon.org>
+"""
+
+__all__ = ['ServiceRegistry']
+
+
+class ServiceRegistry(object):
+    """Representation of the registry visible to services
+
+    Attributes
+    ----------
+    clients : list of ServiceClient
+        Known services
+    """
+    def __init__(self):
+        self._clients = []
+
+    @property
+    def clients(self):
+        """List of clients
+        """
+        raise NotImplementedError()
+        return self._clients
+
+    def locate(self, service_type, service_id=None):
+        """Find a ServiceClient for a specified type
+
+        Parameters
+        ----------
+        service_type : str
+            Type of the service to lookup
+        service_id : int or None
+            If specified, this will look for an exact service_id.
+            Otherwise, this will look for any matching service
+
+        Returns
+        -------
+        service : RegisteredServiceClient
+        """
+        pass
